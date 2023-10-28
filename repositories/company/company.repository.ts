d0cf069549eb068e75b122510504f293d9bcf5bd
MatthieuCollin <101406670+MatthieuCollin@users.noreply.github.com>
2023-10-28 16:19:24 +0200

    📦 NEW: Repo company + type (DONE)

--- repositories/company/company.repository.ts
+++ repositories/company/company.repository.ts
@@ -1,2 +1,68 @@
+
 import { connection } from "../..";
 import { OkPacket } from "mysql2";
+import { Company } from "../../interface/company";
+
+
+interface ICompanyRepository{
+    retrieveAll():Promise<Company[]>
+    retrieveByID(id:string): Promise<Company[]>
+    addCompany(name: string):Promise <Company[]>
+    updateCompany(id: string, name: string):Promise <Company[]>
+    deleteCompany(id: string): Promise<Company[]>
+}
+
+class CompanyRepository implements ICompanyRepository{
+    retrieveAll(): Promise<Company[]> {
+        return new Promise((resolve, reject)=>{
+            let query : string = "SELECT * from companies";
+            connection.query<Company[]>(query, (err, res)=>{
+                if(err) reject (err);
+                else resolve(res)
+            })
+        })
+    };
+
+    retrieveByID(id: string): Promise<Company[]> {
+        return new Promise((resolve, reject)=>{
+            let query : string = `SELECT * from companies WHERE companies.id = ${id}`;
+            connection.query<Company[]>(query, (err, res)=>{
+                if(err) reject (err);
+                else resolve(res);
+            })
+        })
+    };
+
+    addCompany(name: string): Promise<Company[]> {
+        return new Promise((resolve, reject)=>{
+            let query : string = `INSERT INTO companies(name) VALUES ('${name}')`;
+            connection.query<Company[]>(query, (err, res)=>{
+                if(err) reject (err);
+                else resolve(res);
+            })
+        })
+    }
+
+    updateCompany(id: string, name: string): Promise<Company[]> {
+        return new Promise((resolve, reject)=>{
+            let query : string = `UPDATE companies SET companies.name = '${name}' WHERE companies.id = ${id}`
+            connection.query<Company[]>(query, (err, res)=>{
+                if(err) reject (err);
+                else resolve(res);
+            })
+        })
+    }
+
+    deleteCompany(id: string): Promise<Company[]> {
+        return new Promise((resolve, reject)=>{
+            let query : string = `DELETE FROM companies WHERE companies.id = ${id}`;
+            connection.query<Company[]>(query, (err, res)=>{
+                if(err) reject (err);
+                else resolve(res);
+            })
+        })
+    }
+    
+}
+
+export default new CompanyRepository;
--- repositories/type/type.repository.ts
+++ repositories/type/type.repository.ts
@@ -1,2 +1,68 @@
+
 import { connection } from "../..";
 import { OkPacket } from "mysql2";
+import { Type } from "../../interface/type";
+
+
+interface ITypeRepository{
+    retrieveAll():Promise<Type[]>
+    retrieveByID(id:string): Promise<Type[]>
+    addType(name: string):Promise <Type[]>
+    updateType(id: string, name: string):Promise <Type[]>
+    deleteType(id: string): Promise<Type[]>
+}
+
+class TypeRepository implements ITypeRepository{
+    retrieveAll(): Promise<Type[]> {
+        return new Promise((resolve, reject)=>{
+            let query : string = "SELECT * from types";
+            connection.query<Type[]>(query, (err, res)=>{
+                if(err) reject (err);
+                else resolve(res)
+            })
+        })
+    };
+
+    retrieveByID(id: string): Promise<Type[]> {
+        return new Promise((resolve, reject)=>{
+            let query : string = `SELECT * from types WHERE types.id = ${id}`;
+            connection.query<Type[]>(query, (err, res)=>{
+                if(err) reject (err);
+                else resolve(res);
+            })
+        })
+    };
+
+    addType(name: string): Promise<Type[]> {
+        return new Promise((resolve, reject)=>{
+            let query : string = `INSERT INTO types(name) VALUES ('${name}')`;
+            connection.query<Type[]>(query, (err, res)=>{
+                if(err) reject (err);
+                else resolve(res);
+            })
+        })
+    }
+
+    updateType(id: string, name: string): Promise<Type[]> {
+        return new Promise((resolve, reject)=>{
+            let query : string = `UPDATE types SET types.name = '${name}' WHERE types.id = ${id}`
+            connection.query<Type[]>(query, (err, res)=>{
+                if(err) reject (err);
+                else resolve(res);
+            })
+        })
+    }
+
+    deleteType(id: string): Promise<Type[]> {
+        return new Promise((resolve, reject)=>{
+            let query : string = `DELETE FROM types WHERE types.id = ${id}`;
+            connection.query<Type[]>(query, (err, res)=>{
+                if(err) reject (err);
+                else resolve(res);
+            })
+        })
+    }
+    
+}
+
+export default new TypeRepository;
