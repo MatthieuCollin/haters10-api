2f1ba5fc5ed16e180339833f51515c5f1429d557
MatthieuCollin <101406670+MatthieuCollin@users.noreply.github.com>
2023-10-28 16:22:56 +0200

    📦 NEW: Ajout des routes types + companies

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ import express,{ Express } from "express";
 import mysql from "mysql2";
 import bagRepository from './repositories/bag/bag.repository'
 import hatRepository from "./repositories/hat/hat.repository";
+import companyRepository from "./repositories/company/company.repository";
+import typeRepository from "./repositories/type/type.repository";
 
 const app: Express = express();
 
@@ -88,6 +90,80 @@ app.post('/hat/update/:id', function(req, res){
     async function retrieve(){
         let json = req.body;
         let response = await hatRepository.updateHat( req.params.id ,json.name, json.price, json.man, json.woman, json.company, json.type)
+        res.send(response);
+    }
+    retrieve();
+})
+
+// ROUTE COMPANY
+app.get("/companies", function (req, res){
+    async function retrieve(){
+        let response = await companyRepository.retrieveAll();
+        res.send(response)
+    }
+    retrieve();
+})
+
+app.post("/company/add", function(req, res){
+    async function retrieve(){
+        let json = req.body;
+        let response = await companyRepository.addCompany(json.name)
+        res.send(response);
+        
+    }
+    retrieve();
+})
+
+app.delete("/company/delete/:id", function(req, res){
+    async function retrieve(){
+        let response = await companyRepository.deleteCompany(req.params.id);
+        res.send(response);
+    }
+    retrieve();
+});
+
+
+app.post('/company/update/:id', function(req, res){
+    async function retrieve(){
+        let json = req.body;
+        let response = await companyRepository.updateCompany( req.params.id ,json.name)
+        res.send(response);
+    }
+    retrieve();
+})
+
+// ROUTE TYPE
+app.get("/types", function (req, res){
+    async function retrieve(){
+        let response = await typeRepository.retrieveAll();
+        res.send(response)
+    }
+    retrieve();
+})
+
+app.post("/tyoe/add", function(req, res){
+    async function retrieve(){
+        let json = req.body;
+        let response = await typeRepository.addType(json.name)
+        res.send(response);
+        
+    }
+    retrieve();
+})
+
+app.delete("/type/delete/:id", function(req, res){
+    async function retrieve(){
+        let response = await typeRepository.deleteType(req.params.id);
+        res.send(response);
+    }
+    retrieve();
+});
+
+
+app.post('/type/update/:id', function(req, res){
+    async function retrieve(){
+        let json = req.body;
+        let response = await typeRepository.updateType( req.params.id ,json.name)
         res.send(response);
     }
     retrieve();
